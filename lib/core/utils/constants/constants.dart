fddb5a0141069037a932435dc46749d77463679e
mahasachan <mahasachan.sa@ku.th>
2024-04-19 16:56:32 +0700

    add constant color for theming

---
+++ lib/core/utils/constants/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+ColorScheme kColorScheme =
+    ColorScheme.fromSeed(seedColor: Colors.deepOrangeAccent);
+
+ColorScheme kDarkColorScheme =
+    ColorScheme.fromSeed(seedColor: Colors.orangeAccent);
