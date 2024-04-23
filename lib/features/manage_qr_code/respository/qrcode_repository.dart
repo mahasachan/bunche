4b3d638938b4528017fb1d3553f9c1362505d805
mahasachan <mahasachan.sa@ku.th>
2024-04-23 15:44:09 +0700

    remove not use

--- lib/features/manage_qr_code/respository/qrcode_repository.dart
+++
@@ -1,9 +0,0 @@
-import 'package:bunche/data/models/qrcode.dart';
-
-abstract class QRCodeRepository {
-  Future<QRCode> getQRCode(String id);
-  Future<List<QRCode>> getQRCodeList();
-  Future<void> addQRCode(QRCode qrCode);
-  Future<void> updateQRCode(QRCode qrCode);
-  Future<void> deleteQRCode(String id);
-}
