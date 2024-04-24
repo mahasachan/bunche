46c144b40fa777e2a163ac356dffe9980de5dcd5
mahasachan <mahasachan.sa@ku.th>
2024-04-25 04:30:07 +0700

    remove consumer<FriendViewModel>

--- lib/features/manage_qr_code/view/widgets/qrcode_list_preview.dart
+++ lib/features/manage_qr_code/view/widgets/qrcode_list_preview.dart
@@ -1,9 +1,6 @@
 import 'package:bunche/data/datasources/local/hive_qrcode.dart';
 import 'package:bunche/features/manage_qr_code/view/widgets/card_qrcode.dart';
-import 'package:bunche/features/manage_qr_code/view_model/friend_viewmodel.dart';
-// import 'package:bunche/data/models/qrcode.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 class QrcodeListPreview extends StatelessWidget {
   const QrcodeListPreview({super.key, required this.qrcodes});
@@ -12,24 +9,18 @@ class QrcodeListPreview extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // _friendViewModel.qrcodes = qrcodes;
-    return Consumer<FriendViewModel>(
-      builder: (context, FriendViewModel friendViewModel, child) {
-        return SizedBox(
-          // decoration: BoxDecoration(color: Colors.amberAccent.shade100),
-          width: double.infinity,
-          // height: 300,
-          child: ListView.builder(
-              itemCount: friendViewModel.qrcodes.length,
-              itemBuilder: (context, index) {
-                return CardQrcode(
-                  onEditQrcode: () {},
-                  accountName: qrcodes[index].accountName,
-                  qrCode: friendViewModel.qrcodes[index].qrCodeImage,
-                  index: index,
-                );
-              }),
-        );
-      },
+    return SizedBox(
+      width: double.infinity,
+      child: ListView.builder(
+          itemCount: qrcodes.length,
+          itemBuilder: (context, index) {
+            return CardQrcode(
+              onEditQrcode: () {},
+              accountName: qrcodes[index].accountName,
+              qrCode: qrcodes[index].qrCodeImage,
+              index: index,
+            );
+          }),
     );
   }
 
