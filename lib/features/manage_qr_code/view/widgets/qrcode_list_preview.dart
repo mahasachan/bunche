5f7dffe5f51b62cdfd47777be3b5881ffc267176
mahasachan <mahasachan.sa@ku.th>
2024-04-25 16:35:34 +0700

    add condition render CardQrcode

--- lib/features/manage_qr_code/view/widgets/qrcode_list_preview.dart
+++ lib/features/manage_qr_code/view/widgets/qrcode_list_preview.dart
@@ -3,19 +3,26 @@ import 'package:bunche/features/manage_qr_code/view/widgets/card_qrcode.dart';
 import 'package:flutter/material.dart';
 
 class QrcodeListPreview extends StatelessWidget {
-  const QrcodeListPreview({super.key, required this.qrcodes});
+  const QrcodeListPreview({super.key, required this.qrcodes, this.isEdit});
   final List<QRCodeHive> qrcodes;
+  final bool? isEdit;
 
   @override
   Widget build(BuildContext context) {
     // _friendViewModel.qrcodes = qrcodes;
-    return SizedBox(
+    return Container(
+      padding: const EdgeInsets.all(20),
       width: double.infinity,
       child: ListView.builder(
           itemCount: qrcodes.length,
           itemBuilder: (context, index) {
+            if (qrcodes.isEmpty) {
+              return const Center(
+                child: Text('No QR Codes'),
+              );
+            }
             return CardQrcode(
-              onEditQrcode: () {},
+              isEdit: isEdit,
               accountName: qrcodes[index].accountName,
               qrCode: qrcodes[index].qrCodeImage,
               index: index,
@@ -23,7 +30,4 @@ class QrcodeListPreview extends StatelessWidget {
           }),
     );
   }
-
-  void editQrcode(QRCodeHive qrcode) {}
-  //
 }
