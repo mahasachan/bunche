2d71611d0806d2d91b298aa341d02d405d67264b
mahasachan <mahasachan.sa@ku.th>
2024-04-25 16:19:21 +0700

    remove consumer & add parameter to define show in detail or show in edit/create

--- lib/features/manage_qr_code/view/widgets/card_qrcode.dart
+++ lib/features/manage_qr_code/view/widgets/card_qrcode.dart
@@ -8,72 +8,72 @@ class CardQrcode extends StatelessWidget {
     super.key,
     required this.accountName,
     required this.qrCode,
-    required this.onEditQrcode,
     required this.index,
-    // required this.onDeleteQrcode,
+    this.isEdit = false,
   });
 
   final String accountName;
   final Uint8List qrCode;
   final int index;
-  final void Function() onEditQrcode;
+  final bool? isEdit;
+
   // final void Function(int index) onDeleteQrcode;
   // final FriendViewModel friendViewModel;
 
   @override
   Widget build(BuildContext context) {
-    return Consumer<FriendViewModel>(
-      builder: (context, friendViewmodel, child) {
-        return Card(
-          margin: const EdgeInsets.all(10),
-          color: Colors.white,
-          clipBehavior: Clip.antiAlias,
-          elevation: 5,
-          child: Column(
-            children: [
-              ListTile(
-                leading: const Icon(Icons.arrow_drop_down_circle),
-                title: Text(accountName),
-                subtitle: Text(
-                  'Secondary Text',
-                  style: TextStyle(color: Colors.black.withOpacity(0.6)),
-                ),
-              ),
-              Container(
-                decoration: BoxDecoration(color: Colors.blue.shade200),
-                height: 200,
-                width: double.infinity,
-                child: Image.memory(
-                  qrCode,
-                  width: double.infinity,
-                  height: double.infinity,
-                  fit: BoxFit.cover,
-                ),
-              ),
-              ButtonBar(
-                alignment: MainAxisAlignment.end,
-                children: [
-                  TextButton(
-                    onPressed: () {
-                      // Perform some action
-                      debugPrint('action edit');
-                    },
-                    child: const Text('Edit'),
-                  ),
-                  TextButton(
-                    onPressed: () {
-                      debugPrint('action delete');
-                      friendViewmodel.deleteQRCode(index);
-                    },
-                    child: const Text('Delete'),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 10),
-            ],
+    final FriendViewModel friendViewmodel =
+        Provider.of<FriendViewModel>(context);
+    Widget contentBottom = const SizedBox(
+      height: 40,
+    );
+
+    if (isEdit ?? false) {
+      contentBottom = ButtonBar(
+        alignment: MainAxisAlignment.end,
+        children: [
+          TextButton(
+            onPressed: () {},
+            child: const Text('Edit'),
           ),
-        );
-      },
+          TextButton(
+            onPressed: () {
+              friendViewmodel.deleteQRCode(index);
+            },
+            child: const Text('Delete'),
+          ),
+        ],
+      );
+    }
+
+    return Card(
+      color: Colors.white,
+      clipBehavior: Clip.antiAlias,
+      elevation: 5,
+      child: Column(
+        children: [
+          ListTile(
+            title: Text(accountName),
+            subtitle: Text(
+              'Secondary Text',
+              style: TextStyle(color: Colors.black.withOpacity(0.6)),
+            ),
+          ),
+          Container(
+            decoration: BoxDecoration(color: Colors.blue.shade200),
+            height: 200,
+            width: double.infinity,
+            child: Image.memory(
+              qrCode,
+              width: double.infinity,
+              height: double.infinity,
+              fit: BoxFit.cover,
+            ),
+          ),
+          contentBottom,
+          const SizedBox(height: 10),
+        ],
+      ),
     );
   }
 }
