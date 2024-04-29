1578ada7f87229f820115bbce6417df6809aae8b
mahasachan <mahasachan.sa@ku.th>
2024-04-29 10:04:07 +0700

    add singleton FriendList, QrcodeList, GroupList

--- lib/dependency_injection.dart
+++ lib/dependency_injection.dart
@@ -1,6 +1,8 @@
 import 'package:bunche/src/data/models/friend_list.dart';
 import 'package:bunche/src/data/models/group_list.dart';
+import 'package:bunche/src/data/models/qrcode_list.dart';
 import 'package:bunche/src/data/services/friend/friend_service.dart';
+import 'package:bunche/src/data/services/qrcode/qrcode_service.dart';
 import 'package:get_it/get_it.dart';
 
 final singleton = GetIt.instance;
@@ -8,5 +10,7 @@ Future<void> initializeDependencies() async {
   singleton.registerSingleton<FriendList>(FriendList());
   singleton.registerSingleton(FriendService());
 
-  singleton.registerSingleton(GroupList());
+  singleton.registerSingleton<QrcodeList>(QrcodeList());
+  singleton.registerSingleton(QrcodeService());
+  singleton.registerSingleton<GroupList>(GroupList());
 }
